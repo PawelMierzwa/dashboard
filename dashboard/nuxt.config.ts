5b7ff1f9926269e016bd8fdf5ac2258fa887ebf5
Paweł Mierzwa <cxnshx@gmail.com>
2024-05-14 00:51:28 +0200

    Add vuetify

---
+++ dashboard/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    ['@pinia/nuxt',
+      {
+        autoImports: ['defineStore', 'acceptHMRUpdate'],
+      },
+    ],
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+})
