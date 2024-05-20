4057515b8bbafb27b5d2b9004de75015c04614f2
Paweł Mierzwa <cxnshx@gmail.com>
2024-05-21 00:14:09 +0200

    Delete unused handler

--- dashboard/server/index.ts
+++
@@ -1,4 +0,0 @@
-export default defineEventHandler(async () => {
-    const db = useDatabase();
-    await db.sql`CREATE TABLE IF NOT EXISTS users ("id" TEXT PRIMARY KEY, "username" TEXT, "password" TEXT)`;
-});
